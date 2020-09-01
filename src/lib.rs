7d6711a6b7012ee824ddbe0897f018cdb0d3bec1
MaulingMonkey <git@maulingmonkey.com>
2020-09-01 10:55:04 -0700

    lib.rs: forbid(unsafe_code)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(external_doc, feature(external_doc))]
 #![cfg_attr(external_doc, doc(include = "../Readme.md"))]
 #![cfg_attr(external_doc, warn(missing_docs))]
+#![forbid(unsafe_code)]
 
 mod error; pub use error::*;
 #[path = "read/_read.rs"] mod read;   pub use read::*;
