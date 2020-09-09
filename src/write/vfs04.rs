7acd6632bb6cf83c9adf03e18ea527d463941c61
MaulingMonkey <git@maulingmonkey.com>
2020-09-08 17:22:22 -0700

    Fix error matching for zip 0.5.7

--- src/write/vfs04.rs
+++ src/write/vfs04.rs
@@ -43,11 +43,13 @@ impl<IO: Write + Seek + Send + 'static> FileSystem for ZipWriteOnly<IO> {
             imp.dirs.insert(path.into());
             return Ok(());
         }
+        #[allow(unreachable_patterns)] // zip 0.5.3 ..= 0.5.6 didn't have any extra cases
         match imp.writer.add_directory(path, FileOptions::default()) {
             Err(ZipError::FileNotFound)             => return Err(VfsError::FileNotFound { path: path.into() }),
             Err(ZipError::Io(e))                    => return Err(VfsError::IoError(e)),
             Err(ZipError::InvalidArchive(e))        => return Err(VfsError::IoError(io::Error::new(io::ErrorKind::InvalidData, e))),
-            Err(ZipError::UnsupportedArchive(e))    => return Err(VfsError::Other { message: e.into() }),
+            Err(ZipError::UnsupportedArchive(e))    => return Err(VfsError::IoError(io::Error::new(io::ErrorKind::InvalidData, e))),
+            Err(other)                              => return Err(VfsError::Other { message: other.to_string() }),
             Ok(())                                  => {},
         }
         imp.dirs.insert(path.into());
