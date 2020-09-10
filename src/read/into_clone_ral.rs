74da26a93c3eac22884a62bd8d70aab3434c9174
MaulingMonkey <git@maulingmonkey.com>
2020-09-09 17:03:32 -0700

    Improve IntoCloneReadAtLen docs

--- src/read/into_clone_ral.rs
+++ src/read/into_clone_ral.rs
@@ -8,12 +8,19 @@ use std::sync::{Arc, Mutex};
 
 
 
-/// Convert a Mutex\<[Read] + [Seek]\> / Vec\<u8\> / \&\[u8\] / [File] (on most platforms) into a cheaply [Clone]able [ReadAt] + [u64] length.
+/// Convert into a (cheaply!) [Clone]able [ReadAt] + [u64] length.  Implementations include:<br>
+/// [File], [Vec]\<u8\>, [Arc]\<\[u8\]\>, [Box]\<\[u8\]\>, \&\[u8\], [Mutex]\<[Read] + [Seek]\>, and [Arc]\<[Mutex]\<[Read] + [Seek]\>\>.
+///
+/// [ReadAt]:   https://docs.rs/read_write_at/0.1.0/read_write_at/trait.ReadAt.html
 pub trait IntoCloneReadAtLen {
-    /// Some [ReadAt](https://docs.rs/read_write_at/0.1.0/read_write_at/trait.ReadAt.html) implementation.
+    /// Some [Clone]able [ReadAt] implementation.
+    ///
+    /// [ReadAt]:   https://docs.rs/read_write_at/0.1.0/read_write_at/trait.ReadAt.html
     type ReadAt : Clone + ReadAt;
 
     /// Acquire a [Clone]able [ReadAt] + [u64] length
+    ///
+    /// [ReadAt]:   https://docs.rs/read_write_at/0.1.0/read_write_at/trait.ReadAt.html
     fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)>;
 }
 
@@ -31,22 +38,6 @@ impl IntoCloneReadAtLen for File {
     type ReadAt = SeeklessSharedIO<File>;
     fn into_read_at_len(mut self) -> io::Result<(Self::ReadAt, u64)> {
         let len = self.seek(SeekFrom::End(0))?;
-        Ok((SeeklessSharedIO::from(self), len))
-    }
-}
-
-impl<IO: Read + Seek> IntoCloneReadAtLen for Arc<Mutex<IO>> {
-    type ReadAt = SeeklessSharedIO<IO>;
-    fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)> {
-        let len = self.lock().map_err(|_| io::Error::new(io::ErrorKind::Other, "mutex was poisoned"))?.seek(SeekFrom::End(0))?;
-        Ok((SeeklessSharedIO::from(self), len))
-    }
-}
-
-impl<IO: Read + Seek> IntoCloneReadAtLen for Mutex<IO> {
-    type ReadAt = SeeklessSharedIO<IO>;
-    fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)> {
-        let len = self.lock().map_err(|_| io::Error::new(io::ErrorKind::Other, "mutex was poisoned"))?.seek(SeekFrom::End(0))?;
         Ok((SeeklessSharedIO::from(self), len))
     }
 }
@@ -89,5 +80,21 @@ impl<'s> IntoCloneReadAtLen for &'s mut [u8] {
     fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)> {
         let len = self.len() as u64;
         Ok((SeeklessBlob(self), len))
+    }
+}
+
+impl<IO: Read + Seek> IntoCloneReadAtLen for Mutex<IO> {
+    type ReadAt = SeeklessSharedIO<IO>;
+    fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)> {
+        let len = self.lock().map_err(|_| io::Error::new(io::ErrorKind::Other, "mutex was poisoned"))?.seek(SeekFrom::End(0))?;
+        Ok((SeeklessSharedIO::from(self), len))
+    }
+}
+
+impl<IO: Read + Seek> IntoCloneReadAtLen for Arc<Mutex<IO>> {
+    type ReadAt = SeeklessSharedIO<IO>;
+    fn into_read_at_len(self) -> io::Result<(Self::ReadAt, u64)> {
+        let len = self.lock().map_err(|_| io::Error::new(io::ErrorKind::Other, "mutex was poisoned"))?.seek(SeekFrom::End(0))?;
+        Ok((SeeklessSharedIO::from(self), len))
     }
 }
